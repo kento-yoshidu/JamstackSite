717a2da00adbaf2f7fbd5bf8aaeac0f4dd4d41e9
kento yoshidu <kento_yoshidu@yahoo.co.jp>
2020-07-31 23:51:10 +0900

    Create index.html
    トップページの作成と、ルーティング

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hoge')
+  fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  })
 })
 
 const server = http.createServer(app);
