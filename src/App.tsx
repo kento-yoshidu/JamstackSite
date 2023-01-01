d2f0b9a8ba927ffcaafd3053861e9f38c6502450
kento <kento@toriwatari.bird>
2023-01-01 20:08:03 +0900

    ✨ CSS更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
+import Styles from "./styles/style.module.scss"
+
 function App() {
   return (
     <>
       <nav className={Styles.nav}>
         <ul className={Styles.navList}>
-          <li className={Styles.navItem}><Link to="#">HOME</Link></li>
+          <li className={Styles.navItem}>HOME</li>
           <li className={Styles.navItem}><a href="#">Architecture</a></li>
           <li className={Styles.navItem}><a href="#">Sample Sites</a></li>
           <li className={Styles.navItem}><a href="#">Blog Article</a></li>
