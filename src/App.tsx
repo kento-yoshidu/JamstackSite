e38d487ffcfdd39d28e427830afbf7fb82a55f97
kento <kento@toriwatari.bird>
2023-01-01 20:03:58 +0900

    ✨ Install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <nav className={Styles.nav}>
+        <ul className={Styles.navList}>
+          <li className={Styles.navItem}><Link to="#">HOME</Link></li>
+          <li className={Styles.navItem}><a href="#">Architecture</a></li>
+          <li className={Styles.navItem}><a href="#">Sample Sites</a></li>
+          <li className={Styles.navItem}><a href="#">Blog Article</a></li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
