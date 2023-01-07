5522097ae14ac27e581f14092919d03cd4c6c053
kento <kento@toriwatari.bird>
2023-01-07 13:50:51 +0900

    🌠 Install react-ionicons

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className={Styles.allWrapper}>
       <nav className={NavStyles.nav}>
         <ul className={NavStyles.navList}>
-          <li className={NavStyles.navItem}><a href="/">HOME</a></li>
+          <li className={NavStyles.navItem}><a href="#view1">HOME</a></li>
           <li className={NavStyles.navItem}><a href="#">Architecture</a></li>
           <li className={NavStyles.navItem}><a href="#">Sample Sites</a></li>
           <li className={NavStyles.navItem}><a href="#">Blog Article</a></li>
