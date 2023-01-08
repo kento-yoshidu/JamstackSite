0984c724970f217cb7f8a68ebc571c1a3fb3866c
kento <kento@toriwatari.bird>
2023-01-08 09:39:44 +0900

    ✨ Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import ViewList from "./components/ViewList"
 
-import Styles from "./styles/style.module.scss"
+import Styles from "./styles/style.module.css"
 import NavStyles from "./styles/nav.module.css"
 import HeaderStyles from "./styles/header.module.css"
+import ArchitectureStyles from "./styles/architecture.module.css"
 
 function App() {
   return (
@@ -36,6 +37,50 @@ function App() {
 
         <div className={HeaderStyles.scroll}>SCROLL</div>
       </header>
+
+      <section className={`${Styles.section} ${Styles.view2}`} id="view2">
+        <h2 className={Styles.sectionTitle}>Architecture</h2>
+        <div className={ArchitectureStyles.container}>
+          <div className={ArchitectureStyles.box}>
+            <span></span>
+            <div className={ArchitectureStyles.content}>
+              <h3>Morden Web Framework</h3>
+              <p>NextJSやNuxt.js、Gatsbyなどのフレームワークの機能を使い、HTMLやCSSなどの静的ファイルを書き出します。</p>
+            </div>
+          </div>
+
+          <div className={ArchitectureStyles.box}>
+            <span></span>
+            <div className={ArchitectureStyles.content}>
+              <h3>Headless CMS</h3>
+              <p>動的なデータはHeadless CMSサービスを使って管理します。データベースを持つ必要はありません。</p>
+            </div>
+          </div>
+
+          <div className={ArchitectureStyles.box}>
+            <span></span>
+            <div className={ArchitectureStyles.content}>
+              <h3>CDN</h3>
+              <p>静的ファイルはCDNサービスを使って配信します。バックエンドを持たないJamstackならではのメリットです。</p>
+            </div>
+          </div>
+        </div>
+
+        <table className={ArchitectureStyles.archTable}>
+          <tr>
+            <th>SSG</th>
+            <td>NextJS Gatsby Nuxt.js Gridsome SvelteKit</td>
+          </tr>
+          <tr>
+            <th>Headless<br />CMS</th>
+            <td>microCMS Contentful graphCMS prismic</td>
+          </tr>
+          <tr>
+            <th>CDN</th>
+            <td>AWS Amplify Netlify Vercel Gatsby Cloud</td>
+          </tr>
+        </table>
+      </section>
     </div>
   )
 }
